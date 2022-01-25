1abaedf2fcbb7c9cf0462c4db307403a5be82686
EddWills95 <edd.williams@me.com>
2022-01-25 23:23:50 +0000

    add cors to the server

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 
 import { checkEligibility } from "./src/eligibility";
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/", (req, res) => {
   // Check eligibility
